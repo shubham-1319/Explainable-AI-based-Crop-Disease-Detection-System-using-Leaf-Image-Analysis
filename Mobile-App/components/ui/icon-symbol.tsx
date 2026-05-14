81e8b08b13579c41313dec4452ad913d270626d6
Shubham <shubhamkhera428@gmail.com>
2026-05-14 08:00:03 +0100

    error Updated
    glitch updated

--- Mobile-App/components/ui/icon-symbol.tsx
+++ Mobile-App/components/ui/icon-symbol.tsx
@@ -8,11 +8,6 @@ import { OpaqueColorValue, type StyleProp, type TextStyle } from 'react-native';
 type IconMapping = Record<SymbolViewProps['name'], ComponentProps<typeof MaterialIcons>['name']>;
 type IconSymbolName = keyof typeof MAPPING;
 
-/**
- * Add your SF Symbols to Material Icons mappings here.
- * - see Material Icons in the [Icons Directory](https://icons.expo.fyi).
- * - see SF Symbols in the [SF Symbols](https://developer.apple.com/sf-symbols/) app.
- */
 const MAPPING = {
   'house.fill': 'home',
   'paperplane.fill': 'send',
